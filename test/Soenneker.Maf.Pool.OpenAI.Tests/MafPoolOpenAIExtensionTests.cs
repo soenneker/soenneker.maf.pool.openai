aad36ae1296209383f87ddde58f3f2cbb5f968f4
Jake Soenneker <jake@soenneker.com>
2026-02-24 18:38:06 -0600

    misc

--- test/Soenneker.Maf.Pool.OpenAI.Tests/MafPoolOpenAIExtensionTests.cs
+++ test/Soenneker.Maf.Pool.OpenAI.Tests/MafPoolOpenAIExtensionTests.cs
@@ -2,7 +2,6 @@ using Xunit;
 
 namespace Soenneker.Maf.Pool.OpenAI.Tests;
 
-[Collection("Collection")]
 public sealed class MafPoolOpenAIExtensionTests
 {
     public MafPoolOpenAIExtensionTests(ITestOutputHelper output)
