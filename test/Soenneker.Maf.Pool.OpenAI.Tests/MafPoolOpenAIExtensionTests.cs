da6dd47c63e2c6051ac880e102c9681c69aad7bc
Jake Soenneker <jake@soenneker.com>
2026-04-21 19:36:31 -0500

    cleanup

--- test/Soenneker.Maf.Pool.OpenAI.Tests/MafPoolOpenAIExtensionTests.cs
+++ test/Soenneker.Maf.Pool.OpenAI.Tests/MafPoolOpenAIExtensionTests.cs
@@ -3,13 +3,8 @@ namespace Soenneker.Maf.Pool.OpenAI.Tests;
 
 public sealed class MafPoolOpenAIExtensionTests
 {
-    public MafPoolOpenAIExtensionTests( output)
-    {
-    }
-
     [Test]
     public void Default()
     {
-
     }
 }
